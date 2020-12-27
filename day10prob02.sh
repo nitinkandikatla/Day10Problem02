40cf61b41e96320d9291dd3e412f6241e4f10dfc
nitinkandikatla <nitinkandikatla99@gmail.com>
2020-12-27 15:12:49 +0530

    Added UC4 tie condition

--- day10prob02.sh
+++ day10prob02.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-echo "Flip Coin Simulator""
+echo "Flip Coin Simulator"
 head=0
 tail=0
 while [[ $head -lt 21 && $tail -lt 21 ]]
@@ -15,6 +15,25 @@ done
 if [ $head -eq $tail ]
 then
 	echo "Tie"
+	while [ true ]
+	do
+		ran=$(($RANDOM%2+1));
+		if [ $ran -eq 1 ]
+		then
+			((head++))
+		else
+			((tail++))
+		fi
+		d=$(($head-$tail))
+		d1=$(($tail-$head))
+		if [ $d -ge 2 ] || [ $d1 -ge 2 ]
+		then
+			echo "Head;$head Tail:$tail"
+		break
+		else
+			continue
+		fi
+	done
 elif [ $head -gt $tail ]
 then
 	echo "Head won"
